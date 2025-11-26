02ba325cf4a982a6ad8214e47bfbaaf70e2165a2
Ben Brown <benvubrown@gmail.com>
2025-11-25 20:24:13 -0800

    Added sim constants and changed Enum from double to distance

--- src/main/java/frc/robot/RobotConfig.java
+++ src/main/java/frc/robot/RobotConfig.java
@@ -1,11 +1,28 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.units.measure.Distance;
+
 public class RobotConfig {
   public static final class ElevatorConfig {
-    // Elevator measurements (in meters)
-    public static final double kBottomHeight = 0;
-    public static final double kMiddleHeight = 0;
-    public static final double kMaxHeight = 0;
+    // Elevator measurements
+    public static final Distance kL1Height = Meters.of(0.0);
+    public static final Distance kL2Height = Meters.of(0.0);
+    public static final Distance kL3Height = Meters.of(0.0);
+    public static final Distance kL4Height = Meters.of(0.0);
+    public static final Distance kHumanPlayerHeight = Meters.of(0.0);
+    public static final Distance kL2AlgaeHeight = Meters.of(0.0);
+    public static final Distance kL3AlgaeHeight = Meters.of(0.0);
+    public static final int kMotorNumber = 2;
+    public static final double kGearing = 3;
+    public static final double kCarriageMass = 26.914545;
+    public static final double kDrumRadius = 0.0285;
+    public static final double kMinHeight = 0.0; // Meters
+    public static final double kMaxHeight = 1.76; // Meters
+    public static final boolean kSimulateGravity = true;
+    public static final double kStartingHeight = 0.0;
+    public static final double kStandardDeviation = 0.0;
 
     // PID constants
     public static final double kP = 0.1;
@@ -23,21 +40,25 @@ public class RobotConfig {
     public static final double kMaxAcceleration = 0.5;
 
     // Conversion from motor rotations to elevator height
-    public static final double kMetersPerRotation = 0;
+    public static final double kMetersPerRotation = 0.0;
 
     // Elevator heights by state
     public enum ElevatorState {
-      LOW(kBottomHeight),
-      MID(kMiddleHeight),
-      HIGH(kMaxHeight);
+      L1(kL1Height),
+      L2(kL2Height),
+      L3(kL3Height),
+      L4(kL4Height),
+      HUMANPLAYER(kHumanPlayerHeight),
+      L2ALGAE(kL2AlgaeHeight),
+      L3ALGAE(kL3AlgaeHeight);
 
-      private double height;
+      private Distance height;
 
-      ElevatorState(double height) {
+      ElevatorState(Distance height) {
         this.height = height;
       }
 
-      public double getHeight() {
+      public Distance getHeight() {
         return height;
       }
     }
