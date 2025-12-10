55425be82742226538216163d35bc9b842c569f4
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-12-09 16:47:48 -0800

    finished update function

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,12 +2,16 @@ package frc.robot.subsystems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.subsystems.VisionConstants.*;
 
@@ -15,10 +19,12 @@ public class Vision extends SubsystemBase{
 
     private List<CamAndEstimator> cameras = new ArrayList<>();
 
-
+    public Consumer<EstimatedRobotPose> updateDrivetrain;
     public record CamAndEstimator (PhotonPoseEstimator estimator, PhotonCamera camera) {}
 
-    public Vision() {
+    public Vision(Consumer<EstimatedRobotPose> updateDrivetrain) {
+        this.updateDrivetrain = updateDrivetrain;
+
         cameras.add(
             new CamAndEstimator(
                 new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraOffsets.get(0)),
@@ -32,6 +38,50 @@ public class Vision extends SubsystemBase{
                 new PhotonCamera("aprilTwo")
             )
         );
+    }
+
+    public boolean checkTolerance(EstimatedRobotPose pose) {
+        double poseAmbiguity = 1;
+        double distanceAmbiguity = 1;
+
+        for (PhotonTrackedTarget t : pose.targetsUsed) {
+            if (t.poseAmbiguity != -1 && t.poseAmbiguity < poseAmbiguity) {
+                poseAmbiguity = t.poseAmbiguity;
+            }
+
+            double dist =
+            Math.sqrt(
+                Math.pow(t.bestCameraToTarget.getX(), 2)
+                    + Math.pow(t.bestCameraToTarget.getY(), 2));
+
+            if (dist < distanceAmbiguity) {
+                distanceAmbiguity = dist;
+            }
+        }
+
+        if (poseAmbiguity >= AMBIGUITY_TOLERANCE) {
+            return false;
+        }
+        if (distanceAmbiguity >= DISTANCE_TOLERANCE) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public void update() {
+        for (var cameraRecord: cameras) {
+
+            List<PhotonPipelineResult> data = cameraRecord.camera.getAllUnreadResults();
+
+            for (PhotonPipelineResult result: data) {
+                EstimatedRobotPose poseResult = cameraRecord.estimator.update(result).get();
+                if (checkTolerance(poseResult)) {
+                    updateDrivetrain.accept(poseResult);
+                }
+            }
+            
+        }
     }
 
 }
--- src/main/java/frc/robot/subsystems/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/VisionConstants.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Transform3d;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VisionConstants {
-    public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  public static final AprilTagFieldLayout kTagLayout =
+  AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
-    public static List<Transform3d> cameraOffsets = new ArrayList<>();
+  public static final double AMBIGUITY_TOLERANCE = 0.4; // percentage
+  public static final double DISTANCE_TOLERANCE = 2.5; // meters
+
+  public static List<Transform3d> cameraOffsets = new ArrayList<>();
 }
